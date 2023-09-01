8b5cd4b1947aa6b6d0062058a64a1f9e72ab3dc4
danperezma <danperezma@unal.edu.co>
2023-09-01 14:20:37 -0500

    suffix a with methods

--- Strings/Suffix automaton.cpp
+++ Strings/Suffix automaton.cpp
@@ -1,66 +1,84 @@
-/// Complexity: O(|N|*log(|alphabet|))
-/// Tested: https://tinyurl.com/y7cevdeg
-struct suffix_automaton {
-  struct node {
-    int len, link, cnt, first_pos; bool end; /// cnt is endpos size, first_pos is minimum of endpos
-    map<char, int> next;
-  };
-  vector<node> sa;
-  int last;
-  suffix_automaton() {}
-  suffix_automaton(string &s) {
-    sa.reserve(s.size()*2);
-    last = add_node();
-    sa[last].len = sa[last].cnt = sa[last].first_pos = sa[last].end = 0;
-    sa[last].link = -1;
-    for(char c : s) sa_append(c);
-    mark_suffixes();
-    build_cnt();
-  }
-  int add_node() {
-    sa.push_back({});
-    return sa.size()-1;
-  }
-  void mark_suffixes() {
-    ///t0 is not suffix
-    for(int cur = last; cur; cur = sa[cur].link)
-      sa[cur].end = 1;
-  }
-  /// This is O(N*log(N)). Can be done O(N) by doing dfs and counting paths to terminal nodes.
-  void build_cnt() {
-    vector<int> order(sa.size()-1);
-    iota(order.begin(), order.end(), 1);
-    sort(order.begin(), order.end(), [&](int a, int b) { return sa[a].len > sa[b].len; });
-    for(auto &i : order) sa[ sa[i].link ].cnt += sa[i].cnt;
-    sa[0].cnt = 0; /// t0 is empty string 
-  }
-  void sa_append(char c) {
-    int cur = add_node();
-    sa[cur].len = sa[last].len + 1;
-    sa[cur].end = 0; sa[cur].cnt = 1;
-    sa[cur].first_pos = sa[cur].len-1;
-    int p = last;
-    while(p != -1 && !sa[p].next[c] ){
-      sa[p].next[c] = cur;
-      p = sa[p].link;
+struct suffixAutomaton {
+    struct node {
+        int len, link; bool end;
+        map<char, int> next;
+        int cnt; ll in, out;
+    };
+
+    vector<node> sa;
+    int last; ll substrs = 0;
+
+    suffixAutomaton() {}
+    suffixAutomaton(string &s) {
+        sa.reserve(s.size()*2);
+        last = add_node();
+        sa[0].link = -1;
+        sa[0].in = 1;
+        for (char &c : s) add_char(c);
+        for (int p = last; p; p = sa[p].link) sa[p].end = 1;
     }
-    if(p == -1) sa[cur].link = 0;
-    else {
-      int q = sa[p].next[c];
-      if(sa[q].len == sa[p].len+1) sa[cur].link = q;
-      else {
-        int clone = add_node();
-        sa[clone] = sa[q];
-        sa[clone].len = sa[p].len+1;
-        sa[clone].cnt = 0;
-        sa[q].link = sa[cur].link = clone;
-        while(p != -1 && sa[p].next[c] == q) {
-          sa[p].next[c] = clone;
-          p = sa[p].link;
+
+    int add_node() { sa.pb({}); return sa.size()-1; }
+
+    void add_char(char c) {
+        int u = add_node(), p = last;
+        sa[u].len = sa[last].len + 1;
+        while (p != -1 && !sa[p].next.count(c)) {
+            sa[p].next[c] = u;
+            sa[u].in += sa[p].in;
+            substrs += sa[p].in;
+            p = sa[p].link;
         }
-      }
+        if (p != -1) {
+            int q = sa[p].next[c];
+            if (sa[p].len + 1 != sa[q].len) {
+                int clone = add_node();
+                sa[clone] = sa[q];
+                sa[clone].len = sa[p].len + 1;
+                sa[clone].in = 0;
+                sa[q].link = sa[u].link = clone;
+                while (p != -1 && sa[p].next[c] == q) {
+                    sa[p].next[c] = clone;
+                    sa[q].in -= sa[p].in;
+                    sa[clone].in += sa[p].in;
+                    p = sa[p].link;
+                }
+            } else sa[u].link = q;
+        }
+        last = u;
     }
-    last = cur;
-  }
-  node& operator[](int i) { return sa[i]; }
+
+    void run(string &s) {
+        int u = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            while (u && !sa[u].next.count(s[i])) u = sa[u].link;
+            if (sa[u].next.count(s[i])) u = sa[u].next[s[i]];
+        }
+    }
+
+    int match_str(string &s) {
+        int u = 0, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (!sa[u].next.count(s[i])) return 0;
+            u = sa[u].next[s[i]];
+        }
+        return count_occ(u);
+    }
+
+    int count_occ(int u) {
+        if (sa[u].cnt != 0) return sa[u].cnt;
+        sa[u].cnt = sa[u].end;
+        for (auto &v : sa[u].next)
+            sa[u].cnt += count_occ(v.S);
+        return sa[u].cnt;
+    }
+
+    ll count_paths(int u) {
+        if (sa[u].out != 0) return sa[u].out;
+        for (auto &v : sa[u].next)
+            sa[u].out += count_paths(v.S) + 1;
+        return sa[u].out;
+    }
+    
+    node& operator[](int i) { return sa[i]; }
 };
